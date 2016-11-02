b8f65abb9f0f501bd9a896b527b3a9b44b19bb47
pengjoshua <joshuapeng7@gmail.com>
2016-11-02 16:31:47 -0700

    Implemented hash table to spec

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,30 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage[index] === undefined) {
+    this._storage[index] = {};
+    this._storage[index][k] = v;
+  } else {
+    this._storage[index][k] = v;
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage[index] === undefined) {
+    return;
+  } else {
+    return this._storage[index][k];
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage[index] === undefined) {
+    return;
+  } else {
+    this._storage[index][k] = undefined;
+  }
 };
 
 
