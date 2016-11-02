9a409d56ab1c16924fece12f1b84a80af115500c
pengjoshua <joshuapeng7@gmail.com>
2016-11-02 12:05:11 -0700

    Completed pseudo-classical queue to spec

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,24 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.min = 0;
+  this.max = 0;
 };
 
+Queue.prototype.size = function() {
+  return this.max - this.min;
+};
 
+Queue.prototype.dequeue = function() {
+  if (this.size() > 0) {
+    console.log(this.min);
+    var tmp = this[this.min];
+    this[this.min] = undefined;
+    this.min++;
+    return tmp;
+  } else {
+    return;
+  }
+};
+Queue.prototype.enqueue = function(value) {
+  this[this.max] = value;
+  this.max++;
+};
