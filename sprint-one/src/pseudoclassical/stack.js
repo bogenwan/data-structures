bf74b836b991332f1524f3fb274aa8dd08df715a
pengjoshua <joshuapeng7@gmail.com>
2016-11-02 12:10:32 -0700

    Completed pseudoclassical stack to spec

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.height = 0;
+};
+
+Stack.prototype.size = function() {
+  return this.height;
+};
+
+Stack.prototype.push = function(value) {
+  this[this.height] = value;
+  this.height++;
+};
+
+Stack.prototype.pop = function() {
+  if (this.size() > 0) {
+    var tmp = this[this.height - 1];
+    this[this.height - 1] = undefined;
+    this.height--;
+    return tmp;
+  } else {
+    return;
+  }
 };
 
 
