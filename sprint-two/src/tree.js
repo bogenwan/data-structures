51cfa46c70c7ebe4fd19f02ff6f2dd7659c72485
pengjoshua <joshuapeng7@gmail.com>
2016-11-02 20:23:05 -0700

    implemented addChild and contains for tree -- Completed

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,10 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+  for (var key in treeMethods) {
+    newTree[key] = treeMethods[key];
+  }
 
   return newTree;
 };
@@ -11,13 +14,27 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].value === target) {
+      return true;
+    } else {
+      if (this.children[i].contains(target)) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ *
+ * contains O(n) = n
+ * addChild O(n) = 1
  */
