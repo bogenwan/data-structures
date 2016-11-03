0ea53c32ddb6b8cfa09f2ba3d271c81823655713
pengjoshua <joshuapeng7@gmail.com>
2016-11-02 20:28:23 -0700

    Implemented set add, remove, contains to spec

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,27 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = true;
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage[item] === true) {
+    return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  this._storage[item] = undefined;
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * Everything is O(n) = 1
  */
