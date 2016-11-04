1d34fdcf41c124b298a02cfa9107c815a7f0340c
pengjoshua <joshuapeng7@gmail.com>
2016-11-03 20:10:56 -0700

    Implemented hashtable double and halve

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,7 +1,6 @@
-
-
 var HashTable = function() {
   this._limit = 8;
+  this._count = 0;
   this._storage = LimitedArray(this._limit);
 };
 
@@ -16,13 +15,17 @@ HashTable.prototype.insert = function(k, v) {
     bucket[k] = v;
     this._storage.set(index, bucket);
   }
+  this._count++;
+  if (this._count >= (0.75 * this._limit)) {
+    this.double();
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
   if (bucket === undefined) {
-    return;
+    return null;
   } else {
     return bucket[k];
   }
@@ -32,13 +35,52 @@ HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
   if (bucket === undefined) {
-    return;
+    return null;
   } else {
     bucket[k] = undefined;
+    this._count--;
+    if (this._count <= (0.25 * this._limit)) {
+      this.halve();
+    }
   }
 };
 
+HashTable.prototype.double = function() {
+  var holder = [];
+  for (var i = 0; i < this._limit; i++) {
+    var temp = this._storage.get(i);
+    if (temp !== undefined) {
+      holder.push(temp);
+    }
+  }
+  this._limit *= 2;
+  this._storage = LimitedArray(this._limit);
+  for (var i = 0; i < holder.length; i++) {
+    for (var key in holder[i]) {
+      this.insert(key, holder[i][key]);
+    }
+  }
+};
 
+HashTable.prototype.halve = function() {
+  if (this._limit <= 8) {
+    return null;
+  }
+  var holder = [];
+  for (var i = 0; i < this._limit; i++) {
+    var temp = this._storage.get(i);
+    if (temp !== undefined) {
+      holder.push(temp);
+    }
+  }
+  this._limit /= 2;
+  this._storage = LimitedArray(this._limit);
+  for (var i = 0; i < holder.length; i++) {
+    for (var key in holder[i]) {
+      this.insert(key, holder[i][key]);
+    }
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
