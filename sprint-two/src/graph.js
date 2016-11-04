941d7fe24a965db3cc65816c5fba1787cd7369aa
pengjoshua <joshuapeng7@gmail.com>
2016-11-03 19:42:21 -0700

    removeNode now checks if node exists

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -20,6 +20,9 @@ Graph.prototype.contains = function(node) {
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  if (this.contains(node) === false) {
+    return null;
+  }
   for (var key in this[node]) {
     this[key][node] = undefined;
   }
